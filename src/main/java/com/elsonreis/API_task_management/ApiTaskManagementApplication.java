8391a7384ce68a6716b29552f27e228bdaab76f1
elsonreis77 <elsonreis084@gmail.com>
2026-01-19 23:53:57 -0300

    chore: initial Spring Boot project setup

---
+++ src/main/java/com/elsonreis/API_task_management/ApiTaskManagementApplication.java
@@ -0,0 +1,13 @@
+package com.elsonreis.API_task_management;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiTaskManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiTaskManagementApplication.class, args);
+	}
+
+}
